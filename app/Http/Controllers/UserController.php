70dcdc19c914fe297d037fd55354c16b8270a328
Tiara Mera S <merasifa@gmail.com>
2025-02-24 22:45:33 +0700

    praktikum 6

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function profile($id, $name) {
-        return view('user.profile', ['id' => $id, 'name' => $name]);
+    public function index()
+    {
+        // tambah data user dengan Eloquent Model
+        $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'), // Hashing password
+            'level_id' => 4
+        ];
+
+        UserModel::insert($data); // tambahkan data ke tabel m_user
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\PenjualanController;
 use App\Http\Controllers\ProductController;
